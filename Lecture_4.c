ad22bdb9e1b27c25f298dfb8b2e4350a595526c4
dunnywei <wei_don@yahoo.com>
2018-06-29 16:36:52 -0400

    Lecture_4 done

--- Lecture_4.c
+++ Lecture_4.c
@@ -3,7 +3,13 @@
 //First Step
 #include<linux/module_param.h>
 //Second step
-int param_var=0;
+//passing a value
+//int param_var=0;
+//end of passing a value
+
+//passing an array
+//int param_var[3]=[1,2,3];
+//end of passing an array
 //Third step
 /*
 (0:37)
@@ -19,12 +25,30 @@ S_IXGRP
 
 OR S_IRUSR|S_IWUSR (0:51)
 */
-module_param(param_var,int,S_IRUSR|S_IWUSR);
+//passing a value
+//module_param(param_var,int,S_IRUSR|S_IWUSR);
+//end of passing a value
+//passing an array
+module_param_array(param_var,int,NULL,S_IRUSR|S_IWUSR);
+//->(3:07)
+//end of passing an array
+
 
 void display(void)
 {
-  printk(KERN_ALERT "TEST: parameter is %d ",param_var); 
+  //passing a value	
+  //printk(KERN_ALERT "TEST: parameter is %d ",param_var);
   //(2:13)
+  //end of passing a value
+	
+    //passing an array
+    printk(KERN_ALERT "TEST: parameter is %d ",param_var[0]);
+    printk(KERN_ALERT "TEST: parameter is %d ",param_var[1]);
+    printk(KERN_ALERT "TEST: parameter is %d ",param_var[2]);
+    //(3:34)
+    //end of passing an array
+
+
 }
 
 static int hello_init(void)
@@ -42,3 +66,13 @@ static void hello_exit(void)
 
 module_int(hello_init);
 module_exit(hello_exit);
+
+/*
+->https://www.youtube.com/playlist?list=PLZDevgBJtN8zyXV2QaozxzJzgJ9tpZY6p
+->helpful lnnk->https://www.tldp.org/LDP/lkmpg/2.6/html/index.html
+->Now we want to pass an array into the kernel (3:00)
+->Let's go back to console and rebuild (3:39)
+->type the following->"insmod Lecture4.ko param=24,535,19" in CLI (4:02)
+->type the following->"dmesg"->(4:09)
+->type the following->"rmmod Lecture4.ko"->(4:20)
+ */
