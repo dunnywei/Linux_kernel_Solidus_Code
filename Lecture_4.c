a74667d4d1014cbe072635d2add4599f72f07a4b
dunnywei <wei_don@yahoo.com>
2018-06-29 11:56:52 -0400

    update on Lecture_4.c for passing a value before passing an array

--- Lecture_4.c
+++ Lecture_4.c
@@ -1,1 +1,44 @@
-#include <linux/init.h>
+#include<linux/init.h>
+#include<linux/module.h>
+//First Step
+#include<linux/module_param.h>
+//Second step
+int param_var=0;
+//Third step
+/*
+(0:37)
+module_param(name_var,type,permission)
+
+permission can be
+S_IRUSR
+S_IWUSR
+S_IXUSR
+S_IWGRP
+S_IRGRP
+S_IXGRP
+
+OR S_IRUSR|S_IWUSR (0:51)
+*/
+module_param(param_var,int,S_IRUSR|S_IWUSR);
+
+void display(void)
+{
+  printk(KERN_ALERT "TEST: parameter is %d ",param_var); 
+  //(2:13)
+}
+
+static int hello_init(void)
+{
+	printk(KERN_ALERT "TEST:Hello world");
+  display();
+	return 0;
+}
+
+static void hello_exit(void)
+{
+	printk(KERN_ALERT "TEST:End of the Hello world");
+	return 0;
+}
+
+module_int(hello_init);
+module_exit(hello_exit);
