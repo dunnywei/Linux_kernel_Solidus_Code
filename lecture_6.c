c248fdf86885b9e5c926abb925b4ad30b71bd1f9
dunnywei <wei_don@yahoo.com>
2018-07-03 21:19:30 -0400

    Lecture_6 (1:28)

--- lecture_6.c
+++ lecture_6.c
@@ -1,10 +1,13 @@
-/*
+/*Lecture_6
 Two different type of device drivers
 
 Character Devices:
 -Writing/reading character by character
--Opertaing in the block mode;synchrnous with operation
--Most common of all device driver
+-Opertaing in the block mode(0:37)
+   ->It means that it is an atomic operation. When the user writes on the device, user has to wait until the
+device finish writing the last byte before cotinue the user code (0:48)
+-synchrnous mode of operation(Same as block mode)
+-Most common of all device driver (1:26)
 
 Block Device
 -Writing/reading block by block
