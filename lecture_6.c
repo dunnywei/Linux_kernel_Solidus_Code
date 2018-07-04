e2491da28f1e9363535d8552e2ecd81fb7ec29a6
dunnywei <wei_don@yahoo.com>
2018-07-03 21:12:15 -0400

    lecture_6 (0:16)

---
+++ lecture_6.c
@@ -0,0 +1,12 @@
+/*
+Two different type of device drivers
+
+Character Devices:
+-Writing/reading character by character
+-Opertaing in the block mode;synchrnous with operation
+-Most common of all device driver
+
+Block Device
+-Writing/reading block by block
+-CPU intensive, Operation take long time to complete, asynchorous with opoeration
+*/
